716ad1c5a725d3ad5702bf1ca610268fd306be66
JonasErtl <nilshofmeier@gmail.com>
2024-03-06 17:10:59 +0100

    added calculation funciton for second page

--- src/components/Validator1.jsx
+++ src/components/Validator1.jsx
@@ -10,15 +10,14 @@ const [value2, setValue2] = useState();
 const [value3, setValue3] = useState();
 const [value4, setValue4] = useState();
 const [value5, setValue5] = useState();
-const [value6, setValue6] = useState();
 
 const [sum, setSum] = useState(0);
 const [sum2, setSum2] = useState(0);
 
 useEffect(() => {
  setSum(parseFloat(value1) * parseFloat(value3) + parseFloat(value2) * parseFloat(value4)); 
- setSum2(parseFloat(value1) * parseFloat(value5) + parseFloat(value2) * parseFloat(value6));
-}, [value1, value2, value3, value4, value5, value6]);
+ setSum2((parseFloat(value1) + parseFloat(value2)) * parseFloat(value5));
+}, [value1, value2, value3, value4, value5]);
 
 let range = 0.1
 let message = ''
@@ -38,9 +37,9 @@ if (sum - sum2 < range && sum2-sum < range){ //Hardcoded Values sind vielleicht 
 
   return (
     <>
-      <form action="https://p-seminar-validator.netlify.app"><input type="submit" value="Elastischer Stoß" /></form>
+      <form action="https://p-seminar-validator.netlify.app"><input type="submit" value="Inelastischer Stoß" /></form>
       <p>Für die Impulserhaltung gilt:</p>
-      <Latex>$m_1 \cdot v_1 + m_2 \cdot v_2 = m_1 \cdot v_1^\prime + m_2 \cdot v_2^\prime$</Latex><br></br>
+      <Latex>$m_1 \cdot v_1 + m_2 \cdot v_2 = \left( m_1 + m_2 \right) \cdot u$</Latex><br></br>
       <Latex>$m_1$ = </Latex>
       <input type="number" value={value1} onChange={e => setValue1(e.target.value)}/>
       <Latex>$m_2$ = </Latex>
@@ -49,10 +48,8 @@ if (sum - sum2 < range && sum2-sum < range){ //Hardcoded Values sind vielleicht 
       <input type="number" value={value3} onChange={e => setValue3(e.target.value)} />
        <Latex>$v_2$ = </Latex>
       <input type="number" value={value4} onChange={e => setValue4(e.target.value)} /><br></br> 
-       <Latex>$v_1^\prime$ = </Latex>
+       <Latex>$u$ = </Latex>
       <input type="number" value={value5} onChange={e => setValue5(e.target.value)} />
-       <Latex>$v_2^\prime$ = </Latex>
-      <input type="number" value={value6} onChange={e => setValue6(e.target.value)} />
       {!isNaN(sum) && !isNaN(sum2) ? <p>{sum}  {color == 'red' ?  '≠' : '≈'} {sum2}</p>:''}
       <p style={{color: color, border: '1px solid', borderRadius: '10px'}}>{message}</p>
     </>
